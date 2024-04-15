436f722e34d3320385380ce874f3fead76e9b284
Lucas Rouret <lucasro@bam.tech>
2024-04-15 11:55:34 +0200

    feat: rookie-movie-preview-with-rating

--- src/components/MovieCatalog/MovieCatalog.tsx
+++ src/components/MovieCatalog/MovieCatalog.tsx
@@ -1,12 +1,16 @@
 import styles from "./MovieCatalog.module.css";
 
 import GenreList from "../GenreList/GenreList";
+import MoviePreview from "../MoviePreview/MoviePreview";
+import { mockMovie } from "../../mock/mockMovie";
 
 export const MovieCatalog = () => {
   return (
     <>
       <GenreList />
-      <div className={styles.layout}></div>
+      <div className={styles.layout}>
+        <MoviePreview movie={mockMovie} />
+      </div>
     </>
   );
 };
--- src/components/MoviePreview/MoviePreview.tsx
+++ src/components/MoviePreview/MoviePreview.tsx
@@ -1,11 +1,39 @@
+import { POSTER_URL_PREFIX } from "../../App";
+import { Movie } from "../../models";
 import styles from "./MoviePreview.module.css";
 
-const MoviePreview = () => {
+const MoviePreview = ({ movie }: { movie: Movie }) => {
+  const rating = Math.floor((movie.vote_average * 5) / 10);
+
   const goToMovieDetails = () => {
-    // A DEFINIR EXPERT
+    //A DEFINIR EXPERT
   };
 
-  return <div className={styles.movieCard} onClick={goToMovieDetails}></div>;
+  return (
+    <div className={styles.movieCard} onClick={goToMovieDetails}>
+      <div
+        className={styles.moviePoster}
+        style={{
+          backgroundImage: `url(${POSTER_URL_PREFIX + movie.poster_path})`,
+        }}
+      ></div>
+      <div className={styles.movieInfo}>
+        <div className={styles.movieTitle}>{movie.title}</div>
+        <div className={styles.starRating}>
+          {[...Array(5)].map((_, index) => (
+            <span
+              key={index}
+              className={`${styles.star} ${
+                index < rating ? styles.orange : ""
+              }`}
+            >
+              ★
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MoviePreview;
