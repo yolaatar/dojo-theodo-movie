a3fae196d46430bdf480c19285669457afa53814
Lucas Rouret <lucasro@bam.tech>
2024-04-15 13:34:49 +0200

    pro-movie-catalog-without-page

--- src/components/MovieCatalog/MovieCatalog.tsx
+++ src/components/MovieCatalog/MovieCatalog.tsx
@@ -2,14 +2,26 @@ import styles from "./MovieCatalog.module.css";
 
 import GenreList from "../GenreList/GenreList";
 import MoviePreview from "../MoviePreview/MoviePreview";
-import { mockMovie } from "../../mock/mockMovie";
+
+import { useInfiniteQuery } from "react-query";
+import { getMovies } from "../../services/movieService";
 
 export const MovieCatalog = () => {
+  const { data, isLoading } = useInfiniteQuery({
+    queryKey: ["getPopularMovies"],
+    queryFn: ({ pageParam = 1 }) => getMovies(pageParam),
+  });
+
+  const movies = data?.pages.flat() || [];
+
   return (
     <>
       <GenreList />
       <div className={styles.layout}>
-        <MoviePreview movie={mockMovie} />
+        {!isLoading &&
+          movies.map((movie) => <MoviePreview key={movie.id} movie={movie} />)}
+
+        {isLoading && <div>Chargement...</div>}
       </div>
     </>
   );
