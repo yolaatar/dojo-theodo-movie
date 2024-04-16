be19756df95b2861f862097cce43765ecc0f1b4d
Lucas Rouret <lucasro@bam.tech>
2024-04-16 16:15:23 +0200

    hacker-selected-genre

--- src/components/GenreList/GenreList.tsx
+++ src/components/GenreList/GenreList.tsx
@@ -1,18 +1,32 @@
 import { useQuery } from "react-query";
 import styles from "./GenreList.module.css";
 import { getGenres } from "../../services/movieService";
+import { useMovieContext } from "../../context/MovieContext";
 
 const GenreList = () => {
   const { data } = useQuery("getGenres", getGenres);
+  const { selectedGenres, setSelectedGenres } = useMovieContext();
 
   const genres = data || [];
 
-  console.log(genres);
+  const handleGenreClick = (genreId: number) => {
+    if (selectedGenres.includes(genreId)) {
+      setSelectedGenres(selectedGenres.filter((id) => id !== genreId));
+    } else {
+      setSelectedGenres([...selectedGenres, genreId]);
+    }
+  };
 
   return (
     <div className={styles.genreContainer}>
       {genres.map((genre) => (
-        <button key={genre.id} className={`${styles.genre}`}>
+        <button
+          key={genre.id}
+          className={`${styles.genre} ${
+            selectedGenres.includes(genre.id) ? styles.selected : ""
+          }`}
+          onClick={() => handleGenreClick(genre.id)}
+        >
           {genre.name}
         </button>
       ))}
