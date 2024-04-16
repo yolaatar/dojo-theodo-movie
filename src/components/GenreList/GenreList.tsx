702ecb6032f67bb393fccab859baf1bd0d707b33
Lucas Rouret <lucasro@bam.tech>
2024-04-16 08:55:40 +0200

    hacker-genres-in-logs

--- src/components/GenreList/GenreList.tsx
+++ src/components/GenreList/GenreList.tsx
@@ -1,6 +1,14 @@
+import { useQuery } from "react-query";
 import styles from "./GenreList.module.css";
+import { getGenres } from "../../services/movieService";
 
 const GenreList = () => {
+  const { data } = useQuery("getGenres", getGenres);
+
+  const genres = data || [];
+
+  console.log(genres);
+
   return <div className={styles.genreContainer}></div>;
 };
 
