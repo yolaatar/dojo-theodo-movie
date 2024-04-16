0265f47bf46b1c3e3397c4c07cd7749fbbcdd2c7
Lucas Rouret <lucasro@bam.tech>
2024-04-16 08:45:06 +0200

    expert-log-movie-detail

--- src/components/MovieDetails/MovieDetails.tsx
+++ src/components/MovieDetails/MovieDetails.tsx
@@ -1,6 +1,32 @@
+import { useQuery } from "react-query";
 import styles from "./MovieDetails.module.css";
+import { getMovieById } from "../../services/movieService";
+import { useParams } from "react-router-dom";
+import { Movie } from "../../models";
 
 const MovieDetails = () => {
+  let { id: movieId } = useParams();
+
+  if (!movieId) {
+    return <div>Aucun identifiant donné</div>;
+  }
+
+  const { data, isLoading, error } = useQuery("getMovieById", () =>
+    getMovieById(movieId)
+  );
+
+  if (isLoading) {
+    return <p>Chargement ...</p>;
+  }
+
+  if (error) {
+    return <p>Une erreur est survenue</p>;
+  }
+
+  const movie = data as Movie;
+
+  console.log(movie);
+
   return <div className={styles.moviePage}></div>;
 };
 
