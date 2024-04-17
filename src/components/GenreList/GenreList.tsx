714be5d612b9770523333dcc9c4f0f28746063b7
Lucas Rouret <lucasro@bam.tech>
2024-04-17 12:07:25 +0200

    expert-hide-genres-during-searching

--- src/components/GenreList/GenreList.tsx
+++ src/components/GenreList/GenreList.tsx
@@ -5,7 +5,9 @@ import { useMovieContext } from "../../Context/MovieContext";
 
 const GenreList = () => {
   const { data } = useQuery("getGenres", getGenres);
-  const { selectedGenres, setSelectedGenres } = useMovieContext();
+  const { selectedGenres, setSelectedGenres, search } = useMovieContext();
+
+  if (search) return null;
 
   const genres = data || [];
 
