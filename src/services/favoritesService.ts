a88edad454f282d3ceba09fb14ae319f0367cdd7
Lucas Rouret <lucasro@bam.tech>
2024-04-17 13:03:41 +0200

    craftman-favoites-service

--- src/services/favoritesService.ts
+++ src/services/favoritesService.ts
@@ -1,19 +1,26 @@
 const FAVORITES_KEY = "favorites";
 
 export const getFavorites = (): number[] => {
-  //A DEFINIR HACKER
-  return [];
+  const favoritesString = localStorage.getItem(FAVORITES_KEY);
+  const favorites = favoritesString ? JSON.parse(favoritesString) : [];
+  return favorites;
 };
 
 export const addFavorite = (movieId: number) => {
-  //A DEFINIR HACKER
+  const favorites = getFavorites();
+  if (!favorites.includes(movieId)) {
+    favorites.push(movieId);
+    localStorage.setItem(FAVORITES_KEY, JSON.stringify(favorites));
+  }
 };
 
 export const removeFavorite = (movieId: number) => {
-  //A DEFINIR HACKER
+  const favorites = getFavorites();
+  const newFavorites = favorites.filter((id: number) => id !== movieId);
+  localStorage.setItem(FAVORITES_KEY, JSON.stringify(newFavorites));
 };
 
-export const isMovieFavorite = (movieId: number): boolean => {
-  //A DEFINIR HACKER
-  return false;
+export const isMovieFavorite = (movieId: number) => {
+  const favorites = getFavorites();
+  return favorites.includes(movieId);
 };
