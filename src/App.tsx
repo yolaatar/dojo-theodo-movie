75f6b7d3ead996eeeb1032dd6494749df45fe57b
Lucas Rouret <lucasro@bam.tech>
2024-04-24 07:07:59 +0200

    fix: migrate react query v4 to v3

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 import { MovieProvider } from "./context/MovieContext";
 import { Header } from "./components/Header/Header";
 import { MovieCatalog } from "./components/MovieCatalog/MovieCatalog";
